37e6464058ff3b1a949af6d8a8765bd1cc567798
Gustav Müller-Franzes <gustav.mueller-franzes@rwth-aachen.de>
2024-11-14 17:59:57 +0100

    simplify model selection

--- odelia/models/resnet.py
+++ odelia/models/resnet.py
@@ -1,18 +1,25 @@
 from odelia.models import BasicClassifier
 import monai.networks.nets as nets
 import torch 
+import torch.nn as nn 
+import torchvision.models as models
+from einops import rearrange
 
+
+
+def _get_resnet_monai(model):
+    return {
+        18: nets.resnet18, 34: nets.resnet34, 50: nets.resnet50, 101: nets.resnet101, 152: nets.resnet152
+    }.get(model)
 
 class ResNet(BasicClassifier):
     def __init__(
         self, 
         in_ch, 
-        out_ch, 
+        out_ch=1, 
         spatial_dims=3,
-        block='basic',
-        layers=[3, 4, 6, 3],
-        block_inplanes=[64, 128, 256, 512],
-        feed_forward=True,
+        model = 34,
+        resnet_kwargs = {},
         loss=torch.nn.BCEWithLogitsLoss, 
         loss_kwargs={}, 
         optimizer=torch.optim.AdamW, 
@@ -23,8 +30,9 @@ class ResNet(BasicClassifier):
         acc_kwargs={"task":"binary"}
     ):
         super().__init__(in_ch, out_ch, spatial_dims, loss, loss_kwargs, optimizer, optimizer_kwargs, lr_scheduler, lr_scheduler_kwargs, aucroc_kwargs, acc_kwargs)
-        self.model = nets.ResNet(block, layers, block_inplanes, spatial_dims, in_ch, 7, 1, False, 'B', 1.0, out_ch, feed_forward, True)
-   
+        Model = _get_resnet_monai(model)
+        self.model = Model(n_input_channels=in_ch, spatial_dims=spatial_dims, num_classes=out_ch, **resnet_kwargs)
+
     def forward(self, x_in, **kwargs):
         pred_hor = self.model(x_in)
         return pred_hor
