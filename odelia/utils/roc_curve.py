0b022efa86be5fae8d58a6eb666af0f45b714852
Gustav Müller-Franzes <gustav.mueller-franzes@rwth-aachen.de>
2024-11-14 16:26:35 +0100

    improve

--- odelia/utils/roc_curve.py
+++ odelia/utils/roc_curve.py
@@ -2,11 +2,10 @@ import numpy as np
 from sklearn.metrics import roc_curve, auc, confusion_matrix
 import matplotlib
 
-def plot_roc_curve(y_true, y_score, axis, bootraping=1000, drop_intermediate=False, fontdict={}):
-    # ----------- Bootstrapping ------------
+def auc_bootstrapping(y_true, y_score, bootstrapping=1000, drop_intermediate=False):
     tprs, aucs, thrs = [], [], []
     mean_fpr = np.linspace(0, 1, 100)
-    rand_idxs = np.random.randint(0, len(y_true), size=(bootraping, len(y_true))) # Note: with replacement 
+    rand_idxs = np.random.randint(0, len(y_true), size=(bootstrapping, len(y_true))) # Note: with replacement 
     for rand_idx in rand_idxs:
         y_true_set = y_true[rand_idx]
         y_score_set = y_score[rand_idx]
@@ -16,17 +15,24 @@ def plot_roc_curve(y_true, y_score, axis, bootraping=1000, drop_intermediate=Fal
         aucs.append(auc(fpr, tpr))
         optimal_idx = np.argmax(tpr - fpr)
         thrs.append(thresholds[optimal_idx])
+    return tprs, aucs, thrs, mean_fpr
 
-    mean_tpr = np.mean(tprs, axis=0)
+
+
+def plot_roc_curve(y_true, y_score, axis, bootstrapping=1000, drop_intermediate=False, fontdict={}, name='ROC', color='b', show_wp=True):
+    # ----------- Bootstrapping ------------
+    tprs, aucs, thrs, mean_fpr = auc_bootstrapping(y_true, y_score, bootstrapping, drop_intermediate)
+
+    mean_tpr = np.nanmean(tprs, axis=0)
     mean_tpr[-1] = 1.0        
-    std_tpr = np.std(tprs, axis=0, ddof=1)
+    std_tpr = np.nanstd(tprs, axis=0, ddof=1)
     tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
     tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
 
     # ------ Averaged based on bootspraping ------
-    mean_auc = np.mean(aucs)
-    std_auc = np.std(aucs, ddof=1)
- 
+    mean_auc = np.nanmean(aucs)
+    std_auc = np.nanstd(aucs, ddof=1)
+  
 
     # --------- Specific Case -------------
     fprs, tprs, thrs = roc_curve(y_true, y_score, drop_intermediate=drop_intermediate)
@@ -41,12 +47,13 @@ def plot_roc_curve(y_true, y_score, axis, bootraping=1000, drop_intermediate=Fal
     
 
 
-    axis.plot(fprs, tprs, color='b', label=rf"ROC (AUC = {auc_val:.2f} $\pm$ {std_auc:.2f})",
+    axis.plot(fprs, tprs, color=color, label=rf"{name} (AUC = {auc_val:.2f} $\pm$ {std_auc:.2f})",
                 lw=2, alpha=.8)
     axis.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2, label=r'$\pm$ 1 std. dev.')
-    axis.hlines(y=opt_tpr, xmin=0.0, xmax=opt_fpr, color='g', linestyle='--')
-    axis.vlines(x=opt_fpr, ymin=0.0, ymax=opt_tpr, color='g', linestyle='--')
-    axis.plot(opt_fpr, opt_tpr, color='g', marker='o') 
+    if show_wp:
+        axis.hlines(y=opt_tpr, xmin=0.0, xmax=opt_fpr, color='g', linestyle='--')
+        axis.vlines(x=opt_fpr, ymin=0.0, ymax=opt_tpr, color='g', linestyle='--')
+    axis.plot(opt_fpr, opt_tpr, color=color, marker='o') 
     axis.plot([0, 1], [0, 1], linestyle='--', color='k')
     axis.set_xlim([0.0, 1.0])
     axis.set_ylim([0.0, 1.0])
@@ -67,6 +74,7 @@ def plot_roc_curve(y_true, y_score, axis, bootraping=1000, drop_intermediate=Fal
             child.set_color('#dddddd')
  
     return tprs, fprs, auc_val, thrs, opt_idx, conf_matrix
+
 
 
 def cm2acc(cm):
